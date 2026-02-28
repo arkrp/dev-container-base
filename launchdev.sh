2e810d0cba03551342d2534d2efa5245511c8cbe
Hannah Nelson <nexec64@gmail.com>
2026-02-27 16:05:29 -0800

    whole config directory now gets mounted to container. This will make adding new configs easier!

--- launchdev.sh
+++ launchdev.sh
@@ -2,7 +2,7 @@
 project_directory=$(realpath $(dirname $BASH_SOURCE))
 sudo docker run -d -p 9000:22 -p 3939:3939\
     -v workspace:/home/hannahnelson/workspace\
-    -v $project_directory/config/ssh:/app/ssh_keys\
+    -v $project_directory/config:/app/config\
     --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined\
     --hostname dev --name dev dev-container-base
 sleep 0.3
